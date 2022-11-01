7e02c20a8ec7a1dc6817257885ad5def951a492c
tugbafidann <tugbafidan4545@gmail.com>
2022-11-01 23:09:42 +0300

    first commit

---
+++ src/A011.java
@@ -0,0 +1,5 @@
+public class A011 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/A02.java
@@ -0,0 +1,5 @@
+public class A02 {
+    public static void main(String[] args) {
+        System.out.println("A02 class i");
+    }
+}
